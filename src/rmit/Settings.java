1d0e0b93a44df71d39ce3d68e81e8cc4ce8b09e3
Sam King <samuel-king@live.com.au>
2020-03-29 23:16:39 +1100

    initial commit

---
+++ src/rmit/Settings.java
@@ -0,0 +1,19 @@
+package rmit;
+
+public class Settings {
+	public static String project_path = "E:/5.Programming/IDE-Workspaces/Eclipse-General/BDM_A2";//change to local directory.
+	
+	//Bug here - path not found?
+	public static String rtree_index_location = project_path + "index/poi";
+	
+	public static String trip_ids = project_path + "index/la_trips.txt";
+	public static String points_location = project_path + "index/la_points.txt";
+	public static String db_name = "triphobo";
+	public static String db_user = "root";
+	public static String db_pass = "Mike9910!";	
+	//sudo apt-get install mysql-server
+	//mysql -u root -p
+	//create database triphobo
+	//exit
+	//mysql -u root -p triphobo < /home/sheng/workspace/iknn/src/db/tb_la_dataset.sql
+}
